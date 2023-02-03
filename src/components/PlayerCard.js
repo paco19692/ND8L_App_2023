ae03e30df4a8e6a16c6a1dac842676714c61da32
paco19692 <fjohnson926@gmail.com>
2023-02-03 18:22:19 -0500

    Adding fetching records when edit record is pressed

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
+
 const PlayerCard = ({ player }) => {
   return (
     <div className="player-card">
       <h3>{player.playername}</h3>
       <p>{player.teamname}</p>
       <div className="newhandicap">{player.Nhandicap}</div>
+      <div className="buttons">
+        <Link to={"/" + player.id}>
+          <i className="material-icons">edit</i>
+        </Link>
+      </div>
     </div>
   );
 };
--- src/pages/Update.js
+++ src/pages/Update.js
@@ -1,9 +1,89 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import supabase from "../config/supabaseClient";
+
 const Update = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [playername, setPlayername] = useState("");
+  const [teamname, setTeamname] = useState("");
+  const [Nhandicap, setNhandicap] = useState("");
+  const [Ohandicap, setOhandicap] = useState("");
+  const [Ptotalpoints, setPtotalpoints] = useState("");
+
+  useEffect(() => {
+    const fetchPlayer = async () => {
+      const { data, error } = await supabase
+        .from("players")
+        .select()
+        .eq("id", id)
+        .single();
+
+      if (error) {
+        navigate("/", { replace: true });
+      }
+      if (data) {
+        setPlayername(data.playername);
+        setTeamname(data.teamname);
+        setNhandicap(data.Nhandicap);
+        setOhandicap(data.Ohandicap);
+        setPtotalpoints(data.Ptotalpoints);
+        console.log(data);
+      }
+    };
+
+    fetchPlayer();
+  }, [id, navigate]);
+
   return (
     <div className="page update">
-      <h2>Update</h2>
-    </div>
-  )
-}
+      <form>
+        <label htmlFor="playername">Player Name:</label>
+        <input
+          type="text"
+          id="playername"
+          value={playername}
+          onChange={(e) => setPlayername(e.target.value)}
+        />
 
-export default Update
+        <label htmlFor="teamname">Team Name:</label>
+        <textarea
+          id="teamname"
+          value={teamname}
+          onChange={(e) => setTeamname(e.target.value)}
+        />
+
+        <label htmlFor="Nhandicap">New Handicap:</label>
+        <input
+          type="number"
+          id="Nhandicap"
+          value={Nhandicap}
+          onChange={(e) => setNhandicap(e.target.value)}
+        />
+
+        <label htmlFor="Ohandicap">Old Handicap:</label>
+        <input
+          type="number"
+          id="Ohandicap"
+          value={Ohandicap}
+          onChange={(e) => setOhandicap(e.target.value)}
+        />
+
+        <label htmlFor="Ptotalpoints">Players total points:</label>
+        <input
+          type="number"
+          id="Ptotalpoints"
+          value={Ptotalpoints}
+          onChange={(e) => setPtotalpoints(e.target.value)}
+        />
+
+        <button>Update Player Record</button>
+
+        {/*formError && <p className="error">{formError}</p>*/}
+      </form>
+    </div>
+  );
+};
+
+export default Update;
